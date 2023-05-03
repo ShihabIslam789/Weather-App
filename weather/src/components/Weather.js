22c86d0caedf2ad26d4e2e52f550151b94b645ef
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-05-03 14:17:55 -0400

    .js file starter.

---
+++ weather/src/components/Weather.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import DisplayWeather from "./DisplayWeather";
+import "./weather.css";
+
+function Weather() {
+  const [weather, setWeather] = useState([]);
+  const [form, setForm] = useState({
+    city: "",
+    country: "",
+  });
