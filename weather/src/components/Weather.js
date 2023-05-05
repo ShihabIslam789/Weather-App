209f4b70354f1aa33223a87bc32a7a3896042a2a
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-05-05 18:32:57 -0400

    added open api library  to fetrch data from.

--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -8,3 +8,14 @@ function Weather() {
     city: "",
     country: "",
   });
+
+  const APIKEY = "Enter Your API Key";
+  
+  async function weatherData(e) {
+    e.preventDefault();
+    if (form.city === "") {
+      alert("Add values");
+    } else {
+      const data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=${APIKEY}`
+      )
