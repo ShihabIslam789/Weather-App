f5fafb45d00c83fca6d72496c6ccfca29cc3344e
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-05-15 21:26:10 -0400

    weatheritem display.

---
+++ weather/src/components/Weatheritem.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Skycons from "react-skycons"
+import Moment from "moment"
+import NumberEasing from "react-number-easing"
+import { getIcon } from "./../utils/getIcon"
+
+export default class weatherItem extends React.Component {
+    displayName: "WeatherItem"
+  
