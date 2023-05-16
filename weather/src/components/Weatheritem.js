eacefe52d69cd37e1a1d5b0f8d8a5f3bb49d1edd
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-05-15 21:29:31 -0400

    information ordering.

--- weather/src/components/Weatheritem.js
+++ weather/src/components/Weatheritem.js
@@ -7,3 +7,12 @@ import { getIcon } from "./../utils/getIcon"
 export default class weatherItem extends React.Component {
     displayName: "WeatherItem"
   
+    render() {
+        let weather = this.props.data,
+            country = weather.country,
+            city = weather.city,
+            timestamp = weather.dayWeather.dt,
+            temperature = weather.dayWeather.temp.max,
+            weatherType = weather.dayWeather.weather[0].description,
+            weatherDescription = weather.dayWeather.weather[0].main,
+            icon = weather.dayWeather.weather[0].id;
